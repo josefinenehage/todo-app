72d3e9825a2a71dcbda91fef443467a55350050b
josefin.enehage <josefin.enehage@outlook.com>
2019-04-02 14:58:26 +0200

    all functionality working

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ var app = new Vue ({
     el: "#app", 
     data: {
         todoList: [
-            "cook", "clean"
+            "cook", "clean", "Code", "Add your own todo below"
         ],
-        message: ''
+        message: '', 
+        trashcan: '🗑️'
     },
     methods:{
         add(){
+            if(this.message != ''){
             this.todoList.push(this.message);
+            this.message= ''
+        }
+    
+        },
+        remove(index){
+            this.todoList.splice(index, 1);
         }
     }
 })
